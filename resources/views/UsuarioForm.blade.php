b4acce4a01f15cb5153e51f5330fbe25559e73c7
vitorls27 <vitor.ls27@aluno.ifsc.edu.br>
2023-05-17 18:45:50 -0300

    Arrumado o form

--- resources/views/UsuarioForm.blade.php
+++ resources/views/UsuarioForm.blade.php
@@ -11,8 +11,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
-@extends("layouts.app");
-
 @php
     if (!empty($usuario->id)) {
         $route = route('usuario.update', $usuario->id);
@@ -20,8 +18,6 @@
         $route = route('usuario.store');
     }
 @endphp
-
-@section()
 
 <body>
     <div class="container">
