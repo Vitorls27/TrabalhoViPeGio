1f4d3b5e69d6253b6ef00233e532583a73696339
Giordano <giordanoparisotto65@gmail.com>
2023-05-26 14:27:12 -0300

    AJUSTES LOGIN

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Esse email não existe em nosso sistema. Tente novamente',
+    'password' => 'Senha incorreta. Tente novamente',
+    'throttle' => 'Muitas tentativas. Aguarde para tentar novamente.',
 
 ];
--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
+    'reset' => 'Senha atualizada.',
+    'sent' => 'Acesse o link que enviamos por email para redefinir sua senha.',
+    'throttled' => 'Aguarde para tentar',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Não encontramos nenhum usuário com esse email.",
 
 ];
